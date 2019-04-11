0dfe777e5945bfc3b7a01e6584c301ad598ac4d6
fuzzbin <fuzzbin@gmail.com>
2019-04-11 11:39:21 +0200

    Oppdatert

---
+++ Oppgave_3/script.js
@@ -0,0 +1,14 @@
+
+class Instrument {
+    constructor(navn, bilde, lyd){
+        this.navn = navn;
+        this.bilde = bilde;
+        this.lyd = lyd;
+    }
+}
+
+class Spørsmål {
+    constructor(instrument) {
+
+    }
+}
